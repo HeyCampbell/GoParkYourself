2d72deef2aa02609039aa7d4c7b232757e115bed
Jon Campbell <heycampbell@gmail.com>
2015-02-13 11:43:32 -0500

    add factories for signs and spots

---
+++ spec/factories/signs.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :sign do
+    status_order 'P-255161'
+    sign_sequence 4
+    distance 13
+    arrow 'S'
+    sign_description '3 HOUR PARKING 6PM-10PM MON THRU FRI 10AM-10PM SATURDAY W/ SINGLE ARROW'
+  end
+end
---
+++ spec/factories/spots.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :spot do
+    lat 40.705765
+    long -74.007659
+    sign_id
+    user_id 1
+    parked_at
+    remind? true
+    expiration '3 HOUR PARKING 6PM-10PM MON THRU FRI 10AM-10PM SATURDAY W/ SINGLE ARROW'
+  end
+end
+
