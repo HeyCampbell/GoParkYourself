b970378f9543fb5e92b57c23971cfc51f80da534
Kevin Perumal <kevin.perumal@gmail.com>
2015-02-12 21:35:34 -0500

    some seed data, not ready for use yet

---
+++ db/convert.rb
@@ -0,0 +1,30 @@
+# require 'csv'
+# require 'date'
+
+# class Block
+
+#   attr_accessor :borough_code, :status_order_number, :main_st, :from_st, :to_street, :side_of_street
+
+#   def initialize(hash)
+#     @borough_code = hash[:boroughcode]
+#     @status_order = hash[:statusordernumber]
+#     @main_street = hash[:mainstreet]
+#     @from_street = hash[:fromstreet]
+#     @to_street = hash[:tostreet]
+#     @side_of_street = hash[:sideofstreet]
+#   end
+# end
+
+# class Sign
+
+#   attr_accessor :borough_code, :status_order_number, :sign_sequence, :distance, :arrow_points, :sign_discription
+
+#   def initialize(hash)
+#     @borough_code = hash[:boroughcode]
+#     @status_order = hash[:statusordernumber]
+#     @sign_sequence = hash[:signsequence]
+#     @distance = hash[:distance]
+#     @arrow = hash[:arrowpoints]
+#     @sign_discription = hash[:signdiscription]
+#   end
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative 'convert'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# CSV.foreach("manhattan_blocks_csv.csv", headers: true, header_converters: :symbol).each_with_index do |row,index|
+
+#   if index > 30
+#     StreetSection.create(status_order: row[1], main_street: row[2], from_street: row[3], to_street: row[4], side_of_street: row[5])
+#   end
+
+# end
+
+CSV.foreach("manhattan_signs_csv.csv", headers: true, header_converters: :symbol).each_with_index do |row,index|
+
+  if  StreetSection.exists?(status_order: row[1])
+    Sign.create(status_order: row[1], sign_sequence: row[2], distance: row[3], arrow: row[4], sign_description: row[5])
+  end
+end
+
