b1001307c6f939e69ff31070e2458307b5aa5a00
Jon Campbell <heycampbell@gmail.com>
2015-02-14 18:09:20 -0500

    improve no parking method to return parsed regs

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,7 +2,7 @@ class SpotsController < ApplicationController
   def create
     spot = Spot.create!(latitude: params['latitude'], longitude: params['longitude'])
 
-    spot_info = {spot: spot, sign: spot.get_street_sections[0]}
+    spot_info = {spot: spot, regs: spot.effective_sign.no_parking_times}
 
     # if @spot.save
     #   # @signs = @spot.street_section.signs.all
--- app/models/sign.rb
+++ app/models/sign.rb
@@ -3,7 +3,7 @@ class Sign < ActiveRecord::Base
 
 
   def no_parking_times
-    parktag = "Never Park here "
+    # parktag = "Never Park here "
     parking_regs = {mon: 'No Regulation',
         tue: 'No Regulation',
         wed: 'No Regulation',
@@ -15,26 +15,35 @@ class Sign < ActiveRecord::Base
       if /SANITATION BROOM SYMBOL/.match(self.sign_description)
         if /EXCEPT/.match(self.sign_description)
           unaffected_day = /\bEXCEPT\s+\K\S+/.match(self.sign_description).to_s
-          time_range = /((\d|\d:\d*)-(\d|\d:\d*)[APM]{2})/.match(self.sign_description)
+          time_range = /((\d|\d:\d*)[APM]*-(\d|\d:\d*)[APM]{2})/.match(self.sign_description).to_s
+          am_pm = /[APM]/.match(time_range.to_s)
+          times = time_range.split('-')
+          unless /[APM]/.match(times[0])
+            times[0] += am_pm
+          end
           parking_regs.each do |k,v|
-
             unless k.to_s == unaffected_day[0..2].downcase
-              parking_regs[k] = "#{time_range}"
+              parking_regs[k] = {:start => times[0], :stop => times[1]}
+            else
+              parking_regs[k] = 0
             end
           end
-        parktag = 'Street cleaner comes here'
+
+        # parktag = 'Street cleaner comes here'
         end
+      elsif /ANYTIME/.match(self.sign_description)
+        parking_regs = parking_regs.map {|k,v| {k => {:start => "12:00am", :stop => "12:00am"}}}
       end
     elsif /NO STANDING/.match(self.sign_description)
-      parktag ="No Standing Sign"
+      parking_regs = parking_regs.map {|k,v| {k => {:start => "12:00am", :stop => "12:00am"}}}
     elsif /1 HOUR PARKING/.match(self.sign_description)
-      parktag ="1 hour parking sign"
+      parking_regs =parking_regs.map {|k,v| {k => "1 hour parking"}}
     elsif /Curb Line/.match(self.sign_description) || /Building Line/.match(self.sign_description) || /Property Line/.match(self.sign_description)
-      parktag =''
+      parking_regs =parking_regs.map {|k,v| {k => 0}}
     else
       parktag ="I'm sorry Dave, I can't read that just yet. I'll try harder"
     end
-    parsed_regs = parking_regs.map{|k,v| "#{k}=#{v}"}.join(' & ')
-    "#{parsed_regs}"
+    # parsed_regs = parking_regs.map{|k,v| "#{k}=#{v}"}.join(' & ')
+    parking_regs
   end
 end
