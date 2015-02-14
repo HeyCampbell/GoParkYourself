74b352ffb2d7a1f306c4cdc9f270170d63523477
Ricky Thomas & Amar Singh <pair+ricky-thomas.ummerr@devbootcamp.com>
2015-02-14 11:33:55 -0500

    added tracking file for schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213011523) do
+ActiveRecord::Schema.define(version: 20150214163213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,11 +57,13 @@ ActiveRecord::Schema.define(version: 20150213011523) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
     t.string   "name"
-    t.string   "password_digest"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
