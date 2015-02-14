d367ceb87a2a73043d92a33a1762833b2df011de
Ricky Thomas & Amar Singh <pair+ricky-thomas.ummerr@devbootcamp.com>
2015-02-14 12:32:38 -0500

    added user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it "expects true to equal true" do
+    expect(true).to eq true
+  end
+
+  it 'returns a User object' do
+    expect(build(:user)).to be_a(User)
+  end
+
+  it 'has a name that is a string' do
+    expect(build(:user)[:name]).to be_an_instance_of(String)
+  end
+
+  it 'has a oAuth expiry date that is an instance of ActiveSupport::TimeWithZone' do
+    expect(build(:user)[:oauth_expires_at]).to be_an_instance_of(ActiveSupport::TimeWithZone)
+  end
+
 end
