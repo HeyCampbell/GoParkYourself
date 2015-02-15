31b57eba72480221424336061af172560156da4e
Kevin Perumal <kevin.perumal@gmail.com>
2015-02-15 18:03:43 -0500

    create tweet model

--- db/migrate/20150215220900_create_tweets.rb
+++ db/migrate/20150215220900_create_tweets.rb
@@ -3,7 +3,7 @@ class CreateTweets < ActiveRecord::Migration
     create_table :tweets do |t|
       t.string :content
       t.boolean :suspended?
-      t.timedate :created
+      t.datetime :created
 
     end
   end
