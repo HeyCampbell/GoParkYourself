19050fe421483ec63fc762eb900e5277bf9f6b34
Jon Campbell <heycampbell@gmail.com>
2015-02-15 13:58:16 -0500

    fix spot controller to return a better json object

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,7 +2,7 @@ class SpotsController < ApplicationController
   def create
     spot = Spot.create!(latitude: params['latitude'], longitude: params['longitude'])
     spot_info = {spot: spot, regs: spot.get_signs.map do |k,v|
-        {k => v[0].no_parking_times}
+        {side: k , rules: v[0].no_parking_times}
       end
     }
     render json: spot_info.to_json
