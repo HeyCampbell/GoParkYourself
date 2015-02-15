0f6a9e5dcf947398632802e1bae21bb5ea4a0bf3
Jon Campbell <heycampbell@gmail.com>
2015-02-15 15:20:15 -0500

    upodate specs

--- spec/models/sign_spec.rb
+++ spec/models/sign_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 require 'factory_girl_rails'
 
-describe "Sign" do
+# describe "Sign" do
 
-  xit "should be associated with street section" do
+#   xit "should be associated with street section" do
 
+#   end
+
+
+# end
+
+describe "no_parking_times" do
+
+  let(:sign1){Sign.create(status_order: 'P-051496', sign_sequence: 20, distance: 1124, arrow: 'N', sign_description: "NO PARKING ANYTIME (ARROW) (SUPERSEDED BY R7-40RA DON'T USE R7-40A)")}
+  it "Should return a hash of hashes" do
+    expect(sign1.no_parking_times.first.class).to eq(Hash)
   end
+
+
 
 end
