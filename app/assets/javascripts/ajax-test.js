74731e184f4278e601c9705da9c2f56cd91ecf00
Ricky Thomas & Amar Singh <pair+ricky-thomas.ummerr@devbootcamp.com>
2015-02-15 15:23:37 -0500

    now have javascript objects on front-end

--- app/assets/javascripts/ajax-test.js
+++ app/assets/javascripts/ajax-test.js
@@ -30,9 +30,13 @@ function loadNotSuccessful(response) {
 var View = {}
 
 View.drawParkingStatus = function(spot) {
+<<<<<<< HEAD
   var html = 'Side of Street: '
   html += spot.sideOne.Side
   return html
+=======
+  var html = ""
+>>>>>>> now have javascript objects on front-end
 }
 
 var Spot = function(json) {
