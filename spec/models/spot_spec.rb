647781e4c9707bbd7047a0110ce00c98e179e11c
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-15 14:21:40 -0500

    update spot tests

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 require 'factory_girl_rails'
+require 'byebug'
 
 describe "Spot" do
    let(:created_spot) { Spot.create(latitude: 40.705765, longitude: -74.007659, remind?: true )}
-   let(:sections) {created_spot.get_street_sections}
-   let(:intersection) {created_spot.nearest_intersection}
+   let(:pearl_cedar_spot) {Spot.create(latitude: 40.706270, longitude: -74.007170)}
+   # let(:sections) {pearl_cedar_spot.get_street_sections}
+   let(:intersection) {pearl_cedar_spot.nearest_intersection}
 
   before :each do
     @spot = Spot.new(latitude: 40.705765, longitude: -74.007659, remind?: true )
   end
 
   it "should initialize with latitude coordinate" do
-    expect(@spot.latitude).to eq(40.705765)
+    expect(@spot.latitude.to_f).to eq(40.705765)
   end
 
   it "should initialize with longitude coordinate" do
-    expect(@spot.latitude).to eq(-74.007659)
+    expect(@spot.longitude.to_f).to eq(-74.007659)
   end
 
   describe "#set_address_info" do
     it "should save with an address string pulled from Geocoder bing maps api" do
       @spot.save
-      expect(@spot.full_address.class).to eq("String")
+      expect(@spot.full_address.class).to eq(String)
     end
 
     it "should save with an accurate address pulled from Geocoder" do
@@ -32,17 +34,17 @@ describe "Spot" do
 
     it "should save with a main_street string" do
       @spot.save
-      expect(@spot.main_street.class).to eq("String")
+      expect(@spot.main_street.class).to eq(String)
     end
   end
 
   describe "nearest_intersection" do
     it "should have access to geonames username environment variable" do
-      expect(ENV['GEONAMES_USERNAME'].class).to eq("String")
+      expect(ENV['GEONAMES_USERNAME'].class).to eq(String)
     end
 
     it "should return a geonames intersection object" do
-      expect(intersection.class).to eq("GeoNamesAPI::NearestIntersection")
+      expect(intersection.class).to eq(GeoNamesAPI::NearestIntersection)
     end
 
     it "should return an intersection in New York county (manhattan)" do
@@ -52,33 +54,35 @@ describe "Spot" do
   end
 
   describe "#get_street_sections" do
+
     it "should return two street sections" do
+      sections = pearl_cedar_spot.get_street_sections
       expect(sections.count).to eq(2)
     end
 
     it "should return street section with same mainstreet as spot" do
-      # sections = created_spot.get_street_sections
-      expect(spot.main_street.upcase).to eq(sections[0].main_street.upcase)
+      sections = pearl_cedar_spot.get_street_sections
+      expect(pearl_cedar_spot.main_street.upcase).to eq(sections[0].main_street.upcase)
     end
 
     it "should return street section with same mainstreet as spot" do
-      # sections = created_spot.get_street_sections
-      expect(spot.main_street.upcase).to eq(sections[1].main_street.upcase)
+      sections = pearl_cedar_spot.get_street_sections
+      expect(pearl_cedar_spot.main_street.upcase).to eq(sections[1].main_street.upcase)
     end
   end
 
   describe "#get_signs_for(section)" do
-    it "should return relevant signs for a street section" do
+    xit "should return relevant signs for a street section" do
     end
   end
 
   describe "#get_signs" do
-    it "should return relevant signs for a spot from both sides of street" do
+    xit "should return relevant signs for a spot from both sides of street" do
     end
   end
 
-    it 'should identify correct sign' do
-      expect(spot.sign_description).to eq()
+    xit 'should identify correct signs' do
+      expect(@spot.sign_description).to eq()
     end
 
 end
