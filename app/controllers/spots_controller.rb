cf595d5f08d7498ec94bbc54a015bda581131616
Jon Campbell <heycampbell@gmail.com>
2015-02-16 14:36:58 -0500

    adjust method to get correct sections

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,7 +1,7 @@
 class SpotsController < ApplicationController
   def create
     spot = Spot.create(latitude: params['latitude'], longitude: params['longitude'])
-    spot_info = {spot: spot, alt_side: Tweet.suspended?, regs: spot.get_signs.map do |k,v|
+    spot_info = {spot: spot, suspended: Tweet.suspended?, regs: spot.get_signs.map do |k,v|
         {side: k , rules: v[0].no_parking_times}
       end
     }
