a9454eb726813e8c0ace6a39677d22eecd9a87c7
Kevin Perumal <kevin.perumal@gmail.com>
2015-02-15 20:34:06 -0500

    update controller to include twitter info

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,11 +1,13 @@
 class SpotsController < ApplicationController
   def create
     spot = Spot.create(latitude: params['latitude'], longitude: params['longitude'])
-    spot_info = {spot: spot, regs: spot.get_signs.map do |k,v|
+    spot_info = {spot: spot, alt_side: Tweet.pull_and_save, regs: spot.get_signs.map do |k,v|
         {side: k , rules: v[0].no_parking_times}
       end
     }
+
     p spot_info
+
     render json: spot_info.to_json
 
   end
