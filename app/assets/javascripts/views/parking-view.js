32da9f98a7e9cd5571886d2344b14bbdcd992847
Amar Singh <ummerr@gmail.com>
2015-02-17 17:14:25 -0500

    added a method to return inner html of current spot

--- app/assets/javascripts/views/parking-view.js
+++ app/assets/javascripts/views/parking-view.js
@@ -49,6 +49,11 @@ View.ColorizeWithTime = function(sideOneArray, sideTwoArray) {
 
 }
 
+View.ReturnSpotHtml = function() {
+  $spot = $('#table_ct').html();
+  return $spot;
+}
+
 // View.ColorizeWithTime = function(sideOneArray, sideTwoArray) {
 //   var html = '<tr>'
 //   var timeArray = ["11pm", "10pm", "9pm", "8pm", "7pm", "6pm", "5pm", "4pm", "3pm", "2pm", "1pm", "12pm", "11am", "10am", "9am", "8am", "7am", "6am", "5am", "4am", "3am", "2am", "1am", "12am"]
