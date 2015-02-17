18d1c2e8a86d552e419a76e7e8a3ad0112587b1e
Jon Campbell <heycampbell@gmail.com>
2015-02-17 16:04:37 -0500

    basic user page templating

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -4,4 +4,8 @@ class SpotsController < ApplicationController
     spot_info = {spot: spot, suspended: Tweet.suspended?, regs: spot.regs}
     render json: spot_info.to_json
   end
+
+  def show
+
+  end
 end
