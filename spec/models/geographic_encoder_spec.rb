9c13c768552da0d6a24a9bf84e096de3bc24c835
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-17 11:17:44 -0500

    update spec

--- spec/models/geographic_encoder_spec.rb
+++ spec/models/geographic_encoder_spec.rb
@@ -2,39 +2,64 @@ require 'rails_helper'
 require 'factory_girl_rails'
 
 describe "GeographicEncoder" do
-  let(:encoder) {FactoryGirl.create(:geographic_encoder)}
+  let(:point) {[40.708710, -74.010646]}
+  # let(:encoder) {FactoryGirl.create(:geographic_encoder)}
+
+#instances methods
 
   describe "#initialize" do
     it "should have an encodable object" do
       expect(geographic_encoder.encodable.count).to eq(1)
     end
 
-    it "should be configured with bing API key" do
+    xit "should be configured with bing API key" do
       expect(Geocoder.)
     end
   end
 
   describe "#reverse_lookup(lat, long)" do
-    it "should return an object when given a point of latitude and longitude" do
+    xit "should return an object when given a point of latitude and longitude" do
+      expect(reverse_lookup(point)).to eq()
+    end
+
+    xit "should return an error when presented with coordinates that are not a latitude and longitude" do
       expect().to eq()
     end
 
-    it "should return an error when presented with coordinates that are not a latitude and longitude" do
-      expect().to eq()
+  end
+
+  describe "#encode!" do
+
+    it "should" do
+    end
+
+    it "" do
     end
 
   end
 
-  it "should" do
+  describe "#cross_streets_lookup(mainstreet, sidestreet)" do
+
+    it "should" do
+    end
+
+    it "" do
+    end
+
   end
 
-  it "" do
-  end
+#class methods
 
-  it "" do
-  end
+  describe "self.get_distance_in_feet(point1, point2)" do
 
-  it "" do
+    it "should " do
+
+    end
+
+    it "" do
+
+    end
+
   end
 
 end
