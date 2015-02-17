f5614090e06b463d0192a09a24294692b67505e7
Jon Campbell <heycampbell@gmail.com>
2015-02-17 12:51:55 -0500

    add fields to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217152048) do
+ActiveRecord::Schema.define(version: 20150217171845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 20150217152048) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "image"
   end
 
 end
