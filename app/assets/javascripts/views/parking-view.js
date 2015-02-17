1a7d6008730f022ad484fe0380b3539d6af49478
Amar Singh <ummerr@gmail.com>
2015-02-17 15:15:27 -0500

    removed extraneous function

--- app/assets/javascripts/views/parking-view.js
+++ app/assets/javascripts/views/parking-view.js
@@ -19,31 +19,6 @@ View.ParseRegulations = function(todayRegs) {
   return html
 }
 
-
-View.Colorize = function(sideOneArray, sideTwoArray) {
-  var html = ""
-
-  for (var i = sideOneArray.length - 1; i >= 0; i--) {
-    html += "<tr>"
-    //this is the first column
-    if (sideOneArray[i] === true) {
-      html+= "<td class='can_park'></td>"
-    } else {
-      html+= "<td class='cant_park'></td>"
-    }
-
-    //this is the second column
-    if (sideTwoArray[i] === true) {
-      html+= "<td class='can_park'></td>"
-    } else {
-      html+= "<td class='cant_park'></td>"
-    }
-    html += "<tr>"
-  };
-
-  return html;
-}
-
 View.ColorizeWithTime = function(sideOneArray, sideTwoArray) {
   var html = '<tr>'
   var timeArray = ["11pm", "10pm", "9pm", "8pm", "7pm", "6pm", "5pm", "4pm", "3pm", "2pm", "1pm", "12pm", "11am", "10am", "9am", "8am", "7am", "6am", "5am", "4am", "3am", "2am", "1am", "12am"]
