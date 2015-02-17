7437c44602070be513c80ff2644ebc98ba17de19
Amar Singh <ummerr@gmail.com>
2015-02-17 15:37:35 -0500

    reversed array to incorporate rght times

--- app/assets/javascripts/views/parking-view.js
+++ app/assets/javascripts/views/parking-view.js
@@ -49,10 +49,9 @@ View.ColorizeWithTime = function(sideOneArray, sideTwoArray) {
 
 }
 
-//THIS WORKS, JUST REVERSED
 // View.ColorizeWithTime = function(sideOneArray, sideTwoArray) {
 //   var html = '<tr>'
-//   var timeArray = ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"]
+//   var timeArray = ["11pm", "10pm", "9pm", "8pm", "7pm", "6pm", "5pm", "4pm", "3pm", "2pm", "1pm", "12pm", "11am", "10am", "9am", "8am", "7am", "6am", "5am", "4am", "3am", "2am", "1am", "12am"]
 //   var canParkCounter = 0
 
 //   for (var i = timeArray.length - 1; i >= 0; i--) {
