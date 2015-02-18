f616b902043e280d4edbfd403ac23d3fbab91efa
Amar Singh <ummerr@gmail.com>
2015-02-18 12:59:19 -0500

    added javascript to populate form side

--- app/assets/javascripts/reminder.js
+++ app/assets/javascripts/reminder.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
 
-  $('#reminderModal').click(function(){
+  $('#reminderModal').click(function() {
+    var ajaxRequest = $.ajax({
+      type: 'GET',
+      url: '/spots/current.json'
+    });
+    ajaxRequest.done(function(response){
+      var spot = new Spot(response);
+      var sideOne = spot.sideOne.Side
+      var sideTwo = spot.sideTwo.Side
+      $('#phone-form-update').html(updatePhoneForm(sideOne, sideTwo));
+    });
+  });
 
-  })
+});
 
-})
+
+function updatePhoneForm(sideOne, sideTwo) {
+  var html = ""
+  html += "<input type='radio' name='side-of-street' value='" + sideOne + "'>" + " " + sideOne + " Side <br>"
+  html += "<input type='radio' name='side-of-street' value='" + sideTwo + "'>" + " " + sideTwo + " Side"
+  return html
+}
+
+   // <input type="radio" name="side-of-street" value='sideOne'> SideOne
+   //    <input type="radio" name="side-of-street" value='sideTwo'> SideTwo
