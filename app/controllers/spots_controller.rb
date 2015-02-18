8aae4326529d1f839e679537ebf06d3d8784ae4f
Jon Campbell <heycampbell@gmail.com>
2015-02-17 20:29:09 -0500

    add functionality into the json spot object for time to move and boolean of current parking

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,7 +1,7 @@
 class SpotsController < ApplicationController
   def create
     spot = Spot.create(latitude: params['latitude'], longitude: params['longitude'], user: current_user )
-    spot_info = {spot: spot, suspended: Tweet.suspended?, regs: spot.regs, park_right_now: spot.park_now }
+    spot_info = {spot: spot, suspended: Tweet.suspended?, regs: spot.regs, park_right_now: spot.park_now?, move_by: spot.park_till}
     render json: spot_info.to_json
   end
 
