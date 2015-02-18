31758e40daed1b54cc5fa03da8888f7c0f22019c
Kevin Perumal <kevin.perumal@gmail.com>
2015-02-18 11:55:51 -0500

    add remind to controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,8 +3,15 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
      user.update(phone_number: params[:phone_number].delete(" "))
+     #set spot to remind, set expiration based on info passed
 
-    user.save ? return true : return false
+  if user.save
+    user.spots.last.update(remind?:  true, expiration: )
+# send to user page
+  else
+# kick back, not a valid number
+  end
+
   end
 
 end
