fec65f0691ba582abaedb94e153263f6bcd2cb1c
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-18 17:51:34 -0500

    create spot and reminder update routes

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -10,8 +10,29 @@ class SpotsController < ApplicationController
   end
 
   def update
-     p params
+     # p params
      spot = Spot.find(params[:id])
-     spot.update_attribute(spot)
+
+     if params[:remind?] == true
+      if spot.park_till[0][:side] == params[:side_of_street]
+        expiration = spot.park_till[0][:i_can_park_until]
+      else
+        expiration = spot.park_till[1][:i_can_park_until]
+      end
+        spot.update_attributes(remind?:  true, expiration: expiration)
+        Message.send_text_message({number_to_send_to: "+1" + params[:phone_numer].delete(" ").delete(".").delete("-"), body: "Reminder set. Please remember to move your vehicel by #{expiration}. We'll send you a heads up an hour before"})
+      else
+        spot.update_attributes(remind?: false)
+        redirect_to user_url
+    end
   end
+
+  def destroy
+    spot = Spot.find(params[:id])
+
+    spot.destroy
+    redirect_to user_url
+  end
+
+
 end
