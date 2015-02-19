ea2cf799cab43dc190f621607a4771174c7e8a11
Amar Singh <ummerr@gmail.com>
2015-02-18 19:22:36 -0500

    hides the remind me form side if cannot park there now

--- app/assets/javascripts/reminder.js
+++ app/assets/javascripts/reminder.js
@@ -12,17 +12,24 @@ $(document).ready(function() {
       var sideTwo = spot.sideTwo.Side
       var sideOneMove = response.move_by[0].i_can_park_until
       var sideTwoMove = response.move_by[1].i_can_park_until
-      $('#phone-form-update').html(updatePhoneForm(sideOne, sideOneMove, sideTwo, sideTwoMove));
+      var sideOneParkNow = response.park_right_now[0].i_can_has_park
+      var sideTwoParkNow = response.park_right_now[1].i_can_has_park
+
+      $('#phone-form-update').html(updatePhoneForm(sideOne, sideOneMove, sideOneParkNow, sideTwo, sideTwoMove, sideTwoParkNow));
     });
   });
 
 });
 
 
-function updatePhoneForm(sideOne, sideOneMove, sideTwo, sideTwoMove) {
+function updatePhoneForm(sideOne, sideOneMove, sideOneParkNow, sideTwo, sideTwoMove, sideTwoParkNow) {
   var html = ""
-  html += "<input type='radio' name='side_of_street' value='" + sideOne + "'>" + " " + sideOne + " Side | Park until " + sideOneMove + "<br>"
-  if (sideTwo !== undefined) {
+
+  if (sideOneParkNow === true) {
+    html += "<input type='radio' name='side_of_street' value='" + sideOne + "'>" + " " + sideOne + " Side | Park until " + sideOneMove + "<br>"
+  }
+
+  if (sideTwoParkNow === true) {
     html += "<input type='radio' name='side_of_street' value='" + sideTwo + "'>" + " " + sideTwo + " Side | Park until " + sideTwoMove
   }
   return html
