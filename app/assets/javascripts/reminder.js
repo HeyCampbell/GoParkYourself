08192d3601a54b5612b9d01c6fdcc87ae24bcfac
Amar Singh <ummerr@gmail.com>
2015-02-18 19:06:43 -0500

    added times to reminder

--- app/assets/javascripts/reminder.js
+++ app/assets/javascripts/reminder.js
@@ -10,18 +10,20 @@ $(document).ready(function() {
       console.log(response);
       var sideOne = spot.sideOne.Side
       var sideTwo = spot.sideTwo.Side
-      $('#phone-form-update').html(updatePhoneForm(sideOne, sideTwo));
+      var sideOneMove = response.move_by[0].i_can_park_until
+      var sideTwoMove = response.move_by[1].i_can_park_until
+      $('#phone-form-update').html(updatePhoneForm(sideOne, sideOneMove, sideTwo, sideTwoMove));
     });
   });
 
 });
 
 
-function updatePhoneForm(sideOne, sideTwo) {
+function updatePhoneForm(sideOne, sideOneMove, sideTwo, sideTwoMove) {
   var html = ""
-  html += "<input type='radio' name='side_of_street' value='" + sideOne + "'>" + " " + sideOne + " Side <br>"
+  html += "<input type='radio' name='side_of_street' value='" + sideOne + "'>" + " " + sideOne + " Side | Park until " + sideOneMove + "<br>"
   if (sideTwo !== undefined) {
-    html += "<input type='radio' name='side_of_street' value='" + sideTwo + "'>" + " " + sideTwo + " Side"
+    html += "<input type='radio' name='side_of_street' value='" + sideTwo + "'>" + " " + sideTwo + " Side | Park until " + sideTwoMove
   }
   return html
 }
